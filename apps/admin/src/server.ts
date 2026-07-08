e0003cf2e8fee9b352907512d271287e5a898d84
Wilfried Musanzi <musanziw.m@gmail.com>
2026-07-08 14:29:45 +0200

    fix: update port mappings and script commands for client and admin services

--- apps/admin/src/server.ts
+++ apps/admin/src/server.ts
@@ -41,9 +41,7 @@ app.use(
 app.use((req, res, next) => {
   angularApp
     .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
+    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
     .catch(next);
 });
 
@@ -52,7 +50,7 @@ app.use((req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = process.env['PORT'] || 9000;
+  const port = process.env['PORT'] || 4100;
   app.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
--- apps/client/src/server.ts
+++ apps/client/src/server.ts
@@ -43,9 +43,7 @@ app.use(
 app.use((req, res, next) => {
   angularApp
     .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
+    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
     .catch(next);
 });
 
@@ -54,7 +52,7 @@ app.use((req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env['PORT'] || 4200;
   app.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
